84ffd0162fd7156e83ef5da3fec6dde7a9c6a81a
Teacher <Teacher>
2016-06-28 20:02:02 -0400

    randomly selected colors appear on the grid on mouseover

--- public/js/app.js
+++ public/js/app.js
@@ -5,15 +5,25 @@ $(document).ready(function(){
   var numOfCols = 10;
 
   initGrid();
-  addClickHandlers();
-  function changeColor(){
-    console.log('changing');
+ addClickHandlers();
+//  addClickHandlersV2()
+      function addClickHandlersV2(){
+    var cells = $('.cell');
+    cells.on('click', changeColor);
   }
+
+  function changeColor(){
+    var colorClasses = ['white', 'red', 'green', 'blue'];
+    var colorCycle = Math.round(Math.random() * colorClasses.length);
+    var color = colorClasses[colorCycle];
+    $(this).removeClass(colorClasses.join(' '));
+    $(this).addClass(color);
+    }
   function addClickHandlers(){
     var cells = $('.cell');
     for(var counter = 0; counter < cells.length; counter +=1){
       var cell = cells[counter];
-      $(cell).on('click', changeColor);
+      $(cell).on('mouseenter', changeColor);
     }
 }
     //$('.cell').click(){
