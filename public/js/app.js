96639b5e599ee696925603ae5ea14b0924efc17f
Teacher <Teacher>
2016-06-29 20:53:13 -0400

    added user input

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,20 @@
 
 $(document).ready(function(){
   var container= $('.container');
-  var numOfRows = 10;
-  var numOfCols = 10;
+  var numOfRows = $('#userrow');
+  var numOfCols = $('#usercolumn');
+  var customTable = $('#custom')
 
-  initGrid();
- addClickHandlers();
-//  addClickHandlersV2()
+ customTable.on('click', initGrid);
+ customTable.on('click', addClickHandlersV2);
+
+  addClickHandlersV2()
       function addClickHandlersV2(){
     var cells = $('.cell');
     cells.on('click', changeColor);
   }
+
+
 
   function changeColor(){
     var colorClasses = ['white', 'red', 'green', 'blue'];
@@ -18,24 +22,23 @@ $(document).ready(function(){
     var color = colorClasses[colorCycle];
     $(this).removeClass(colorClasses.join(' '));
     $(this).addClass(color);
-    }
-  function addClickHandlers(){
-    var cells = $('.cell');
-    for(var counter = 0; counter < cells.length; counter +=1){
-      var cell = cells[counter];
-      $(cell).on('mouseenter', changeColor);
-    }
-}
-    //$('.cell').click(){
-    //  $(this).addClass('.cellColor');
-  //  }
-//  };
+     }
+//   function addClickHandlers(){
+//     var cells = $('.cell');
+//     for(var counter = 0; counter < cells.length; counter +=1){
+//       var cell = cells[counter];
+//       $(cell).on('mouseenter', changeColor);
+//     }
+// }
+
+
 
   function initGrid(){
-    for( var i = 0; i< numOfRows; i +=1){
+    $('.container').empty();
+    for( var i = 0; i< numOfRows.val(); i +=1){
       var row = $('<div></div>');
       row.addClass('row');
-      for(var j = 0; j < numOfCols; j+=1){
+      for(var j = 0; j < numOfCols.val(); j+=1){
         var cell = $('<div></div>');
         cell.addClass('cell border');
         row.append(cell);
@@ -43,5 +46,6 @@ $(document).ready(function(){
       }
       container.append(row);
     }
+
   }
 });
